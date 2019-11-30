98916b6f26c0133bccabe616b3cf54798119db9b
xianglai <xianglai@cvte.com>
2019-11-30 10:03:10 +0800

    [Add] Add conanfile.py

---
+++ conanfile.py
@@ -0,0 +1,38 @@
+from conans import ConanFile, CMake, tools
+
+
+class LzstringcppConan(ConanFile):
+    name = "EasyScreenCapturer"
+    version = "1.0.1"
+    license = "MIT"
+    author = "xl"
+    url = "https://github.com/243286065/EasyScreenCapturer"
+    description = "A lib to capture screen on windows or linux."
+    topics = ("Screen capture")
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = {"shared": False}
+    generators = "cmake"
+
+    def configure(self):
+        if self.settings.os == "Linux":
+            self.settings.compiler.libcxx="libstdc++11"
+
+    def source(self):
+        self.run("git clone https://github.com/243286065/EasyScreenCapturer.git")
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder="EasyScreenCapturer")
+        cmake.build()
+
+    def package(self):
+        self.copy("EasyScreenCapturer.h", dst="include", src="EasyScreenCapturer/src")
+        self.copy("*.lib", dst="lib", keep_path=False)
+        self.copy("*.dll", dst="bin", keep_path=False)
+        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.dylib", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["EasyScreenCapturer"]
